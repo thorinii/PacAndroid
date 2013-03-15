968a2d11935e6ffcf92b815edfe24a9e6a02b925
ThorinII <flipsidelachy@gmail.com>
2013-03-15 16:30:58 +1030

    Setup desktop and android

---
+++ common/src/pacandroid/PacAndroidGame.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacandroid;
+
+import com.badlogic.gdx.Game;
+
+/**
+ *
+ * @author lachlan
+ */
+public class PacAndroidGame extends Game {
+
+    @Override
+    public void create() {
+        // TODO: setScreen(new GameScreen(this));
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
